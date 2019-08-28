ab1796114e8960a0a436c89533a013b2e900830b
leshikjanz <leshikjanz@zivitech.com>
2019-08-28 19:40:33 +0300

    refactor: implement something same

--- src/App.js
+++ src/App.js
@@ -7,31 +7,53 @@ import { SLIDES_RESOLUTIONS } from "./Slider/constants";
 
 const Slide = styled.div`
   position: relative;
-  ${({ disabled }) => disabled && "pointer-events: none;"};
-  transform: rotate(${({ index }) => 15 * index}deg);
-  top: ${({ index }) => index * 50 - (index > 10 ? (80 * (index - 10)) : 0)}px;
-  right: ${({ index }) => index * 60 + (index > 10 ? (130 * (index - 10)) : 0)}px;
+  /* transform: rotate(180deg) translateX(150px) rotate(180deg); */
+  transform: rotate(${({ index, degree }) =>
+    180 + index * 20 + degree}deg) translateX(360px) rotate(${({
+  index,
+  degree
+}) => 180 + index * 20 + degree}deg);
+  /* transform: rotate(${({ index }) => 15 * index}deg);
+  top: ${({ index }) => index * 50 - (index > 10 ? 80 * (index - 10) : 0)}px;*/
+  right: ${({ index }) => (index >= 10 ? 60 * (index - 9) : 50 * index - 420)}px; 
 `;
 
-function App() {
-  const images = new Array(50)
-    .fill(0)
-    .map((img, index) => `https://picsum.photos/200/300?image=${110 + index}`);
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <Slider breakpoints={SLIDES_RESOLUTIONS} showSwiperButtons>
-          {images.map((img, index) => (
-            <div key={img} className="slide">
-              <Slide index={index}>
-                <img draggable={false} src={img} alt="" />
-              </Slide>
+class App extends React.Component {
+  state = {
+    degree: 0
+  };
+
+  handleClick = () => {
+    this.setState(prevState => ({ degree: prevState.degree + 2 }));
+  };
+
+  render() {
+    const images = new Array(19)
+      .fill(0)
+      .map(
+        (img, index) => `https://picsum.photos/200/300?image=${110 + index}`
+      );
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="swiper-wrapper">
+            <div className="swiped-area disable-user-select">
+              {images.map((img, index) => (
+                <div key={img} className="slide" name={index}>
+                  <Slide index={index} degree={this.state.degree}>
+                    <img draggable={false} src={img} alt="" />
+                  </Slide>
+                </div>
+              ))}
             </div>
-          ))}
-        </Slider>
+          </div>
+        </div>
+        <button className="moveButton" onClick={this.handleClick}>
+          Move
+        </button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Slider/constants.js
+++ src/Slider/constants.js
@@ -5,7 +5,7 @@ export const MIN_SHADOW_SLIDES_COUNT = 0
 export const ANIMATION_DURATION_MS = 500
 
 export const SLIDES_RESOLUTIONS = {
-  "1200": 27,
+  "1200": 19,
   "1000": 5,
   "800": 4,
   "600": 2,
