b392770121ab2152e49d6922565e9edcf05f9425
leshikjanz <leshikjanz@zivitech.com>
2019-08-28 19:41:26 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,15 +7,11 @@ import { SLIDES_RESOLUTIONS } from "./Slider/constants";
 
 const Slide = styled.div`
   position: relative;
-  /* transform: rotate(180deg) translateX(150px) rotate(180deg); */
-  transform: rotate(${({ index, degree }) =>
-    180 + index * 20 + degree}deg) translateX(360px) rotate(${({
-  index,
-  degree
-}) => 180 + index * 20 + degree}deg);
-  /* transform: rotate(${({ index }) => 15 * index}deg);
-  top: ${({ index }) => index * 50 - (index > 10 ? 80 * (index - 10) : 0)}px;*/
-  right: ${({ index }) => (index >= 10 ? 60 * (index - 9) : 50 * index - 420)}px; 
+  transform: rotate(${({ index, degree }) => 180 + index * 20 + degree}deg)
+    translateX(360px)
+    rotate(${({ index, degree }) => 180 + index * 20 + degree}deg);
+  right: ${({ index }) =>
+    index >= 10 ? 60 * (index - 9) : 50 * index - 420}px;
 `;
 
 class App extends React.Component {
