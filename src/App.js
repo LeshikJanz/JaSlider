f93953be7b0c48ffd6f00fabb4cc62e6499e03da
leshikjanz <leshikjanz@zivitech.com>
2019-08-28 18:29:35 +0300

    j

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
 import logo from "./logo.svg";
 import "./App.css";
 import Slider from "./Slider/Slider";
 import { SLIDES_RESOLUTIONS } from "./Slider/constants";
 
+const Slide = styled.div`
+  position: relative;
+  ${({ disabled }) => disabled && "pointer-events: none;"};
+  transform: rotate(${({ index }) => 15 * index}deg);
+  top: ${({ index }) => index * 50}px;
+  right: ${({ index }) => index * 30}px;
+`;
+
 function App() {
   const images = new Array(50)
     .fill(0)
-    .map((img, index) => `https://picsum.photos/200/300?image=${110 + index}`);
+    .map((img, index) => ({ img: `https://picsum.photos/200/300?image=${110 + index}`, index}))
+    console.log("images", images)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
       <div className="wrapper">
         <Slider breakpoints={SLIDES_RESOLUTIONS} showSwiperButtons>
-          {images.map(img => (
-            <div key={img} className="slide">
-              
-              <img
-                draggable={false}
-                src={img}
-                alt=""
-              />
+          <Fragment>
+          {images.slice(0, 10).map((elem, index) => (
+            <div key={elem.img} className="slide" name={elem.index}>
+              <Slide index={elem.index}>
+                <img draggable={false} src={elem.img} alt="" />
+              </Slide>
             </div>
           ))}
+          </Fragment>
+          <Fragment>
+          {images.slice(10, 20).map((elem, index) => (
+            <div key={elem.img} className="slide" name={elem.index}>
+              <Slide index={elem.index}>
+                <img draggable={false} src={elem.img} alt="" />
+              </Slide>
+            </div>
+          ))}
+          </Fragment>
         </Slider>
       </div>
     </div>
--- src/Slider/constants.js
+++ src/Slider/constants.js
@@ -1,7 +1,7 @@
 // @flow
 
 export const DEFAULT_SLIDES_PER_VIEW = 4
-export const MIN_SHADOW_SLIDES_COUNT = 8
+export const MIN_SHADOW_SLIDES_COUNT = 18
 export const ANIMATION_DURATION_MS = 500
 
 export const SLIDES_RESOLUTIONS = {
