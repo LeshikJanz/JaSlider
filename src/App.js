de00218df7d2ee53726a854849d613a07c96b07b
leshikjanz <leshikjanz@zivitech.com>
2019-08-28 17:44:42 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Slider from "./Slider/Slider"
-import { SLIDES_RESOLUTIONS } from "./Slider/constants"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Slider from "./Slider/Slider";
+import { SLIDES_RESOLUTIONS } from "./Slider/constants";
 
-function App () {
+function App() {
+  const images = new Array(50)
+    .fill(0)
+    .map((img, index) => `https://picsum.photos/200/300?image=${110 + index}`);
   return (
     <div className="App">
       <header className="App-header">
@@ -13,261 +16,16 @@ function App () {
       </header>
       <div className="wrapper">
         <Slider breakpoints={SLIDES_RESOLUTIONS} showSwiperButtons>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=110" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=111" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=112" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=113" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=114" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=115" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=116" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=117" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=118" alt="" />
-            <img draggable={false} src="https://picsum.photos/200/300?image=218" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=119" alt="" />
-            <img draggable={false} src="https://picsum.photos/200/300?image=219" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=120" alt="" />
-            <img draggable={false} src="https://picsum.photos/200/300?image=220" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=121" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=122" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=123" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=124" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=125" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=126" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=127" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=128" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=129" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=130" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=131" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=132" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=133" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=134" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=135" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=136" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=137" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=188" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=139" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=140" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=141" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=142" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=143" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=144" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=145" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=122" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=123" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=124" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=125" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=126" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=127" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=128" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=129" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=130" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=131" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=132" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=133" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=134" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=135" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=136" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=137" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=188" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=139" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=140" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=141" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=142" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=143" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=144" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=145" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=122" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=123" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=124" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=125" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=126" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=127" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=128" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=129" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=130" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=131" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=132" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=133" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=134" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=135" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=136" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=137" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=188" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=139" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=140" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=141" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=142" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=143" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=144" alt="" />
-          </div>
-          <div className="slide">
-            <img draggable={false} src="https://picsum.photos/200/300?image=145" alt="" />
-          </div>
+          {images.map(img => (
+            <div key={img} className="slide">
+              
+              <img
+                draggable={false}
+                src={img}
+                alt=""
+              />
+            </div>
+          ))}
         </Slider>
       </div>
     </div>
